35ac9ba6efec3b327940d22de7634d569aa4260f
Yinwei Zhang <yzhang50@stuy.edu>
2024-11-06 11:13:50 -0500

    relative import fix?!

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,8 @@ import os
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 
-from .blog_routes import *
-from .util_routes import *
+from blog_routes import *
+from util_routes import *
 
 if __name__ == "__main__":
     app.debug = True
